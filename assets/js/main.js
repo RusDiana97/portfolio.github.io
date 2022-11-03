25baf3c214d1ea1b2d11fa27aef80eafdd024c89
Rus Diana <russ.dianav@gmail.com>
2022-11-03 22:56:25 +0200

    create files

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+// OPEN TAB ON CLICK
+let tabLinks = document.getElementsByClassName("tab-links");
+let tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabname) {
+  // get the name tab on click for tab link and tab content
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+
+  // underline on active link
+  event.currentTarget.classList.add("active-link");
+
+  // show current active tab by name tab
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+// SIDE MENU
+let sideMenu = document.getElementById("side-menu");
+
+function openMenu() {
+  sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+  sideMenu.style.right = "-10rem";
+}
+
+// SCROLL REVEAL ANIMATION
+const scrollRev = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 2000,
+  reset: true,
+});
+
+scrollRev.reveal(
+  `.header-description, .subtitle, .about-description, .tab-titles, .tab-contents, .portfolio-list, .btn, .contact-col`,
+  {
+    interval: 200,
+  }
+);
